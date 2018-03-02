9c87455e52597dd348afb5e654d5218c3cb02566
Liubou Shchupak <36640822+veseja@users.noreply.github.com>
2018-03-02 11:19:53 +0300

    sol

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+
+    if (solveOne()) return matrix;
+
+    function solveOne() {
+        for (var i = 0; i < 9; i++) {
+            for (var j = 0; j < 9; j++) {
+                if (matrix[i][j] == 0) {
+                    for (var n = 0; n < 9; n++) {
+                        if (tryPossibility(i, j, n+1) == true) {
+                            matrix[i][j] = n+1;
+                            if (solveOne() == true) return true;
+                            matrix[i][j] = 0;
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    function tryPossibility(i, j, n) {
+        for (var k = 0; k < 9; k++) {
+            if (matrix[i][k] == n) return false;
+        }
+        for (var k = 0; k < 9; k++) {
+            if (matrix[k][j] == n) return false;
+        }
+        for (t = 0; t < 3; t++) {
+            for (u = 0; u < 3; u++) {
+                if (matrix[i - i % 3 + t][j - j % 3 + u] == n) return false;
+            }
+        }
+        return true;
+
+    }
+
 }
